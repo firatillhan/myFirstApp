d36701a2d9cdfb0929e72fb72c70b71e1804a10d
firat ilhan <firatilhan008@gmail.com>
2022-11-07 16:12:33 +0300

    Initial Commit

---
+++ myFirstApp/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  myFirstApp
+//
+//  Created by Fırat İlhan on 6.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var labelText: UILabel!
+    
+    
+    var mesajText = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        labelText.text = ""
+    }
+
+
+    @IBAction func sendBtn(_ sender: Any) { 
+        if textField.text == "" {
+            let alert = UIAlertController(title: "Uyari", message: "Mesaj alanı Boş Bırakılamaz.", preferredStyle: UIAlertController.Style.alert)
+            let ok = UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(ok)
+            present(alert, animated: true, completion: nil)
+           
+        } else {
+            labelText.text = "Mesajınız: \(textField.text!)"
+        }
+    }
+}
+
